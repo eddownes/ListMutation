144d6a43dcb600e0654f6c870f3c0525f5d127ee
Ed Downes <eddownes@outlook.com>
2020-01-09 18:02:12 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+movies = ["Thor", "Avengers", "Hulk"]
+print(movies)
+
+print(len(movies))
+
+for movie in movies:
+  print(movie)
+
+movies.append("Spiderman")
+movies.append("Guardians")
+
+print(movies)
+print(len(movies))
